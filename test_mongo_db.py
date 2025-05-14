39cc8210c1e67cc622d95df70a9ea2ab7db092fb
Rahul Prasad <rahulprasad4work@gmail.com>
2025-05-14 12:44:03 +0530

    push_data added - no use as we inserting via local

---
+++ test_mongo_db.py
@@ -0,0 +1,14 @@
+
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://prasadrahul2002:rahul1234@cluster0.uitnp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
